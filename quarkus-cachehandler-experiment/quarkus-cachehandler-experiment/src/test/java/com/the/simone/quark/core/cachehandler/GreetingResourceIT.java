fd3df35c6e02f49ee24f93c3759811fa6bd0bbd5
PICCIRILLI <simone.a.piccirilli@capgemini.com>
2024-06-19 23:43:00 +0200

    first commit

---
+++ quarkus-cachehandler-experiment/quarkus-cachehandler-experiment/src/test/java/com/the/simone/quark/core/cachehandler/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package com.the.simone.quark.core.cachehandler;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
